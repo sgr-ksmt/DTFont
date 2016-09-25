9efbf6bdcbdbc043cfb34d8dfca3b8ba768924fc
sgr-ksmt <melodydance.k.s@gmail.com>
2016-09-26 00:26:21 +0900

    Setup project

---
+++ DTFont/DTFont.h
@@ -0,0 +1,19 @@
+//
+//  DTFont.h
+//  DTFont
+//
+//  Created by Suguru Kishimoto on 9/26/16.
+//  Copyright © 2016 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTFont.
+FOUNDATION_EXPORT double DTFontVersionNumber;
+
+//! Project version string for DTFont.
+FOUNDATION_EXPORT const unsigned char DTFontVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTFont/PublicHeader.h>
+
+
